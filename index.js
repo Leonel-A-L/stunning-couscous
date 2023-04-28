e94de7c1c6b4cedd1e79bbc7f9d8d8f8107aadfa
Leonel-A-L <lamelas.leonel@gmail.com>
2023-04-28 13:46:53 -0400

    Adding places controller

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ app.get('/', (req,res) => {
     res.send('Hello World')
 })
 
+app.use('/places', require('./controllers/places'))
+
 app.get('*',(req,res) => {
     res.status(404).send('<h1>404 Page </h1>')
 })
