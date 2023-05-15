fc2f8f4b528099e142f180d1398727bd578e8a64
Leonel-A-L <lamelas.leonel@gmail.com>
2023-05-15 17:52:12 -0400

    part 10 in progress

--- controllers/places.js
+++ controllers/places.js
@@ -1,79 +1,3 @@
-// const router = require('express').Router()
-// const places = require('../models/places')
-
-// router.get('/new', (req, res) => {
-//   res.render('places/new')
-// })
-
-
-
-// router.get('/', (req,res) => {   
-//     res.render('places/index', { places })
-// })
-
-
-// router.get('/:index', (req,res) => {    
-//   const { index } = req.params
-//   res.render('show', {
-//       place: places[index],
-//       index
-//   })
-// })
-
-// router.get('/:index/edit', (req,res) => {
-//   const { index } = req.params
-//   res.render('places/edit', {
-//       place: places[index],
-//       index
-//   })
-// })
-
-// router.post('/', (req, res) => {
-//   console.log(req.body)
-//   if (!req.body.pic) {
-//     // Default image if one is not provided
-//     req.body.pic = 'http://placekitten.com/400/400'
-//   }
-//   if (!req.body.city) {
-//     req.body.city = 'Anytown'
-//   }
-//   if (!req.body.state) {
-//     req.body.state = 'USA'
-//   }
-//   places.push(req.body)
-//   res.redirect('/places')
-// })
-
-// router.delete('/:index', (req, res) => {  
-//   const { index } = req.params  
-//   places.splice(index, 1)
-//     res.redirect('/places')     
-// })
-
-// router.put('/:id', (req, res) => {
-//   const { index } = req.params.id 
-//       // Dig into req.body and make sure data is valid
-//       if (!req.body.pic) {
-//           // Default image if one is not provided
-//           req.body.pic = 'http://placekitten.com/400/400'
-//       }
-//       if (!req.body.city) {
-//           req.body.city = 'Anytown'
-//       }
-//       if (!req.body.state) {
-//           req.body.state = 'USA'
-//       }
-
-//       // Save the new data into places[id]
-//       places[index] = req.body
-//       res.redirect(`/places/${index}`)
-  
-// })
-
-
-
-// module.exports = router
-
 const router = require('express').Router()
 const db = require('../models')
 
@@ -119,18 +43,17 @@ router.get('/new', (req, res) => {
 
 router.get('/:id', (req, res) => {
   db.Place.findById(req.params.id)
+  .populate('comments')
   .then(place => {
-    res.render('show', { place })
+      console.log(place.comments)
+      res.render('places/show', { place })
   })
   .catch(err => {
-    console.log('err', err)
-    res.render('error404')
+      console.log('err', err)
+      res.render('error404')
   })
 })
 
-router.put('/:id', (req, res) => {
-  res.send('PUT /places/:id stub')
-})
 
 router.delete('/:id', (req, res) => {
   res.send('DELETE /places/:id stub')
@@ -143,6 +66,28 @@ router.get('/:id/edit', (req, res) => {
 router.post('/:id/rant', (req, res) => {
   res.send('GET /places/:id/rant stub')
 })
+
+router.post('/:id/comment', (req, res) => {
+  console.log(req.body)
+  db.Place.findById(req.params.id)
+  .then(place => {
+      db.Comment.create(req.body)
+      .then(comment => {
+          place.comments.push(comment.id)
+          place.save()
+          .then(() => {
+              res.redirect(`/places/${req.params.id}`)
+          })
+      })
+      .catch(err => {
+          res.render('error404')
+      })
+  })
+  .catch(err => {
+      res.render('error404')
+  })
+})
+
 
 router.delete('/:id/rant/:rantId', (req, res) => {
     res.send('GET /places/:id/rant/:rantId stub')
